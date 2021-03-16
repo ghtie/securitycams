e9f0eaed833b4cb0c0b746656629a2a95ad37ef1
Gloria Tie <tie.h.gloria@gmail.com>
2021-03-16 18:19:57 -0400

    Motion detection and image capture functionality added
    Signed-off-by: Gloria Tie <tie.h.gloria@gmail.com>

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
+import time
 import cv2
+from motion_detection import MotionDetection
 
+
+# Start the cameras
 cam_1 = cv2.VideoCapture(0)
 cam_2 = cv2.VideoCapture(1)
+time.sleep(3)
+
+# Initialize motion detection
+motion_1 = MotionDetection(img_capture='image_captures/cam1/')
+motion_2 = MotionDetection(img_capture='image_captures/cam2/')
 
 while True:
-    # Capture frame-by-frame
-    ret1, frame1 = cam_1.read()
-    ret2, frame2 = cam_2.read()
+    # Camera frames
+    ret_1, frame1 = cam_1.read()
+    ret_2, frame2 = cam_2.read()
 
-    # Our operations on the frame come here
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    # Frame operations
+    # Convert to grayscale
+    gray_1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    gray_2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame1',gray1)
-    cv2.imshow('frame2', gray2)
+    # Reduce image noise
+    gray_1 = cv2.GaussianBlur(gray_1, (21, 21), 0)
+    gray_2 = cv2.GaussianBlur(gray_2, (21, 21), 0)
+
+    # Motion detection
+    motion_1.motion_detection(gray_1)
+    motion_2.motion_detection(gray_2)
+
+    # Display the camera feeds
+    cv2.imshow('Camera Feed 1', gray_1)
+    cv2.imshow('Camera Feed 2', gray_2)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
 cam_1.release()
 cam_2.release()
 cv2.destroyAllWindows()
---
+++ motion_detection.py
@@ -0,0 +1,49 @@
+import cv2
+import imutils
+import numpy as np
+from datetime import datetime
+
+
+class MotionDetection:
+    def __init__(self, threshold=50, img_capture='image_captures/'):
+        # threshold sets the min difference between the avg_img and the current frame
+        self.threshold = threshold
+        self.img_capture = img_capture
+        self.avg_img = None
+
+    def motion_detection(self, img):
+        """
+        Detects any motion in the frame
+        :param img: current image being analyzed
+        """
+        if self.avg_img is None:
+            self.avg_img = img #img.astype("float")
+
+        # get any frame changes
+        #cv2.accumulateWeighted(img, self.avg_img, 0.5)
+        #diff_frames = cv2.absdiff(img, cv2.convertScaleAbs(self.avg_img))
+        diff_frames = cv2.absdiff(img, self.avg_img)
+        thresh_frames = cv2.threshold(diff_frames, self.threshold, 255, cv2.THRESH_BINARY)[1]
+        thresh_frames = cv2.dilate(thresh_frames, None, iterations=2)
+
+        if not np.all(thresh_frames == 0):
+            # get image contours
+            self.motion_capture(img, thresh_frames)
+
+    def motion_capture(self, img, thresh_frames):
+        """
+        Creates screenshots from frames that have detected motion
+        :param img: current image/frame being analyzed
+        :param thresh_frames: ndarray of image pixels
+        """
+        contours = cv2.findContours(thresh_frames, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        for contour in contours:
+            if cv2.contourArea(contour) > 5000:
+                (x, y, w, h) = cv2.boundingRect(contour)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # create screenshots for user to review later
+                now = datetime.now()
+                img_name = self.img_capture + now.strftime("%m-%d-%Y-%H:%M:%S") + '.jpg'
+                cv2.imwrite(img_name, img)
